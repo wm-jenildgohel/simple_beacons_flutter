b3c180d56ea26d5ec0e2a994a9b4578b20b3198e
dovecheng <chenglaian@qq.com>
2025-02-20 17:07:25 +0800

    Update BeaconsBroadcastReceiver.kt

--- android/src/main/kotlin/com/umair/beacons_plugin/BeaconsBroadcastReceiver.kt
+++ android/src/main/kotlin/com/umair/beacons_plugin/BeaconsBroadcastReceiver.kt
@@ -4,8 +4,10 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import io.flutter.view.FlutterMain
-
+//import io.flutter.view.FlutterMain
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.loader.FlutterLoader
+import io.flutter.FlutterInjector
 
 class BeaconsBroadcastReceiver : BroadcastReceiver() {
     companion object {
@@ -13,7 +15,9 @@ class BeaconsBroadcastReceiver : BroadcastReceiver() {
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        FlutterMain.ensureInitializationComplete(context, null)
+        // FlutterMain.ensureInitializationComplete(context, null)
+        FlutterInjector.instance().flutterLoader().startInitialization(context)
+        FlutterInjector.instance().flutterLoader().ensureInitializationComplete(context, null)
         BeaconsPlugin.startBackgroundService(context)
     }
 }
